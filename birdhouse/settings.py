a55626a6fcfbabd7bb1f4fdadd1ca199ec8a267a
Tadpol Rachatasumrit <tadpolr@trmbp.local>
2017-07-12 15:01:57 +0700

    Fix UI with K Arlekk

--- birdhouse/settings.py
+++ birdhouse/settings.py
@@ -83,23 +83,23 @@ WSGI_APPLICATION = 'birdhouse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': constants['BIRDHOUSE_DB_NAME'],
-        'USER': constants['BIRDHOUSE_DB_USER'],
-        'PASSWORD': constants['BIRDHOUSE_DB_PASSWORD'],
-        'HOST': 'localhost',
-        'PORT': '5432',
-    },
-}
-
 # DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': 'mydatabase',
-#    }
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': constants['BIRDHOUSE_DB_NAME'],
+#         'USER': constants['BIRDHOUSE_DB_USER'],
+#         'PASSWORD': constants['BIRDHOUSE_DB_PASSWORD'],
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     },
 # }
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': 'mydatabase',
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
